f98cfbdbba81b2dcb7db2f8d810c6145a9c01a53
partharoy0 <partha.royballave@gmail.com>
2026-05-15 23:35:03 -0400

    Add Vite app source

---
+++ src/components/ui/card.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { cn } from "@/lib/utils";
+
+export function Card({ className, ...props }) {
+  return <div className={cn("border shadow-sm", className)} {...props} />;
+}
+
+export function CardContent({ className, ...props }) {
+  return <div className={cn("p-6", className)} {...props} />;
+}
